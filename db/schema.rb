fb13f637e5f324020ba89cfd21722c95443ab2b8
Austin Baker <secondbaker@Austins-MacBook.local>
2020-05-30 18:20:22 -0400

    The class is in the database.  I'm going to add two indeces:  source and destination.  This table will have to have one join for each direction, so I think I'll make a special BlogPost#link_to(BlogPost) function for that, to make sure that I do that consistently.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_210623) do
+ActiveRecord::Schema.define(version: 2020_05_30_220335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 2020_05_30_210623) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "name"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "internal_links", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
