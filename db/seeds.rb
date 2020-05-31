734cb493802b225f01fb2d4e083f5b5a57a3d8e8
Austin Baker <secondbaker@Austins-MacBook.local>
2020-05-31 12:31:14 -0400

    First the seed file has to destroy everything

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+InternalLink.destroy_all
+BlogPost.destroy_all
+
