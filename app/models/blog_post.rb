3ddf90797d02c4633c84e491b5cac1bd486a463c
Austin Baker <secondbaker@Austins-MacBook.local>
2020-05-31 11:48:51 -0400

    BlogPosts can now be added as another BlogPost's :source or :destination.  Next I'll make a function to link them both at once.

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,2 +1,7 @@
 class BlogPost < ApplicationRecord
+    has_many :internal_links, foreign_key: 'source_id'
+    has_many :sources, through: :internal_links, foreign_key: 'source_id', class_name: 'BlogPost'
+    has_many :destinations, through: :internal_links, foreign_key: 'destination_id', class_name: 'BlogPost'
+
+    
 end
--- app/models/internal_link.rb
+++ app/models/internal_link.rb
@@ -1,2 +1,4 @@
 class InternalLink < ApplicationRecord
+    belongs_to :source, class_name: 'BlogPost', optional: true
+    belongs_to :destination, class_name: 'BlogPost', optional: true
 end
