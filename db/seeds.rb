b3d46f2a97f4be2269a38138cdbf2fd5e35a4d3e
Austin Baker <secondbaker@Austins-MacBook.local>
2020-05-31 13:23:34 -0400

    Got the seed working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+verbose = true
+
+if verbose
+    puts 'Destroying InternalLink'
+end
 InternalLink.destroy_all
+if verbose
+    puts 'Destroying BlogPost'
+end
 BlogPost.destroy_all
 
 150.times do
-    post = BlogPost.create()
+    name = Faker::Color.unique.hex_color
+    if verbose
+        puts name
+    end
+    body = ''
+    rand(1..15).times do
+        body += Faker::Games::Pokemon.name + ' '
+    end
+    if verbose
+        puts body
+    end
+    post = BlogPost.create(name: name, body: body)
+    rand(0..[15, BlogPost.all.size].min).times do
+        post.link BlogPost.order('RANDOM()').where.not(id: post.id).first
+    end
 end
+
+puts 'Created ' + BlogPost.all.size.to_s + ' BlogPosts'
+puts 'With ' + InternalLink.all.size.to_s + ' InternalLinks'
