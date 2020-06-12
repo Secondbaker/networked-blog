8433bd10abb3d67595c167ed4ce8563dbd08539d
secondbaker <secondbaker@gmail.com>
2020-06-12 10:16:11 -0400

    The function is still undefined, but at least conversions is loading now.

---
+++ app/javascript/conversions.js
@@ -0,0 +1,6 @@
+console.log('hello from conversions');
+
+function blogPostToCyNodeArray(blogPost) {
+	console.log(blogPost);
+}
+function internalLinkToCyEdgeArray(internalLink) {}
--- app/javascript/packs/mini_graph.js
+++ app/javascript/packs/mini_graph.js
@@ -1,26 +1,31 @@
-var cytoscape = require("cytoscape");
-var coseBilkent = require("cytoscape-cose-bilkent");
+var cytoscape = require('cytoscape');
+var coseBilkent = require('cytoscape-cose-bilkent');
 let dagre = require('cytoscape-dagre');
 var fcose = require('cytoscape-fcose');
 
-cytoscape.use( fcose );
-cytoscape.use( coseBilkent );
-cytoscape.use( dagre ); // register extension
+cytoscape.use(fcose);
+cytoscape.use(coseBilkent);
+cytoscape.use(dagre); // register extension
 
-var $ = require("jquery")
+var $ = require('jquery');
 
-var allPosts = []
+var allPosts = [];
 allPosts.concat(gon.related_posts);
 allPosts.push(gon.blog_post);
 
-var elements = []
-elements.concat(gon.related_posts);
+var elements = [];
+elements = elements.concat(gon.related_posts);
 elements.push(gon.blog_post);
-elements.concat(gon.internal_links);
+elements = elements.concat(gon.internal_links);
 
 console.log(elements);
+console.log(gon.related_posts);
 
 var cy = cytoscape({
-  container: $('#cy'),
-  elements: elements
+	container: $('#cy'),
+	elements: elements,
 });
+
+console.log(cy.data());
+
+console.log(blogPostToCyNodeArray(cy.data()[0]));
