75153f52f70f5087edf519dfb30b571156cbd40c
secondbaker <secondbaker@gmail.com>
2020-06-16 10:14:39 -0400

    The mini-graph is showing and has all the nodes.  Next I need to show the labels.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,114 +1,218 @@
 # frozen_string_literal: true
 
+
+
 class BlogPostsController < ApplicationController
+
   before_action :set_blog_post, only: %i[show edit update destroy]
 
+
+
   # GET /blog_posts
+
   # GET /blog_posts.json
+
   def index
+
     @blog_posts = BlogPost.order('LENGTH(body) asc')
+
   end
+
+
 
   # GET /blog_posts/1
+
   # GET /blog_posts/1.json
+
   def show
+
     @internal_links = @blog_post.internal_links
 
-    @related_posts = @blog_post.destinations
-    destinations = @blog_post.destinations
-    @related_posts.each do |related_post|
-      @internal_links = @internal_links.merge related_post.internal_links
-      destinations = destinations.merge related_post.destinations
-    end
 
-    @related_posts = destinations
+
+    @related_posts = @blog_post.destinations
+
+
+
+    @related_posts.merge @blog_post.sources
+
+
 
     gon.related_posts = @related_posts
+
     gon.blog_post = @blog_post
+
     gon.internal_links = @internal_links
+
   end
+
+
 
   def graph
+
     @blog_posts = BlogPost.all
+
     @internal_links = InternalLink.all
+
     @graph_data = []
+
     @blog_posts.each do |blog_post|
+
       data_hash = { data: { id: blog_post.id } }
+
       @graph_data.push data_hash
+
     end
+
     @internal_links.each do |internal_link|
+
       data_id = internal_link.source_id.to_s + '' + internal_link.destination_id.to_s
+
       source_id = internal_link.source_id
+
       target_id = internal_link.destination_id
+
       data_hash = { data: { id: data_id, source: source_id, target: target_id } }
+
       @graph_data.push data_hash
+
     end
+
     gon.graph_data = @graph_data
+
     gon.blog_posts = @blog_posts
+
     gon.internal_links = @internal_links
+
     gon.max_links = BlogPost.max_links
+
     gon.min_links = BlogPost.min_links
+
     gon.max_body_length = BlogPost.max_body_length
+
     gon.min_body_length = BlogPost.min_body_length
+
   end
+
+
 
   # GET /blog_posts/new
+
   def new
+
     @blog_post = BlogPost.new
+
   end
+
+
 
   # GET /blog_posts/1/edit
+
   def edit; end
 
+
+
   # POST /blog_posts
+
   # POST /blog_posts.json
+
   def create
+
     @blog_post = BlogPost.new(blog_post_params)
 
+
+
     respond_to do |format|
+
       if @blog_post.save
+
         format.html { redirect_to @blog_post, notice: 'Blog post was successfully created.' }
+
         format.json { render :show, status: :created, location: @blog_post }
+
       else
+
         format.html { render :new }
+
         format.json { render json: @blog_post.errors, status: :unprocessable_entity }
+
       end
+
     end
+
   end
+
+
 
   # PATCH/PUT /blog_posts/1
+
   # PATCH/PUT /blog_posts/1.json
+
   def update
+
     respond_to do |format|
+
       if @blog_post.update(blog_post_params)
+
         format.html { redirect_to @blog_post, notice: 'Blog post was successfully updated.' }
+
         format.json { render :show, status: :ok, location: @blog_post }
+
       else
+
         format.html { render :edit }
+
         format.json { render json: @blog_post.errors, status: :unprocessable_entity }
+
       end
+
     end
+
   end
 
+
+
   # DELETE /blog_posts/1
+
   # DELETE /blog_posts/1.json
+
   def destroy
+
     @blog_post.destroy
+
     respond_to do |format|
+
       format.html { redirect_to blog_posts_url, notice: 'Blog post was successfully destroyed.' }
+
       format.json { head :no_content }
+
     end
+
   end
+
+
 
   private
 
+
+
   # Use callbacks to share common setup or constraints between actions.
+
   def set_blog_post
+
     @blog_post = BlogPost.find(params[:id])
+
   end
 
+
+
   # Only allow a list of trusted parameters through.
+
   def blog_post_params
+
     params.require(:blog_post).permit(:name, :body)
+
   end
+
 end
+
--- app/javascript/packs/mini_graph.js
+++ app/javascript/packs/mini_graph.js
@@ -4,42 +4,110 @@ var cytoscape = require('cytoscape');
 var coseBilkent = require('cytoscape-cose-bilkent');
 let dagre = require('cytoscape-dagre');
 var fcose = require('cytoscape-fcose');
-var conversions = require('conversions');
 cytoscape.use(fcose);
 cytoscape.use(coseBilkent);
 cytoscape.use(dagre); // register extension
 
 var $ = require('jquery');
 
-var elements = [];
-elements.push(blogPostToCyNodeArray(gon.blog_post));
-gon.related_posts.forEach(function (post) {
-	elements.push(blogPostToCyNodeArray(post));
-	console.log(post.id);
-});
-gon.internal_links.forEach(function (internalLink) {
-	console.log(internalLink.source_id);
-	console.log(internalLink.destination_id);
-	elements.push(internalLinkToCyEdgeArray(internalLink));
-});
+var defaultfCoseOptions = {
+	name: 'fcose',
+	// 'draft', 'default' or 'proof'
+	// - "draft" only applies spectral layout
+	// - "default" improves the quality with incremental layout (fast cooling rate)
+	// - "proof" improves the quality with incremental layout (slow cooling rate)
+	quality: 'default',
+	// Use random node positions at beginning of layout
+	// if this is set to false, then quality option must be "proof"
+	randomize: true,
+	// Whether or not to animate the layout
+	animate: true,
+	// Duration of animation in ms, if enabled
+	animationDuration: 1000,
+	// Easing of animation, if enabled
+	animationEasing: undefined,
+	// Fit the viewport to the repositioned nodes
+	fit: true,
+	// Padding around layout
+	padding: 30,
+	// Whether to include labels in node dimensions. Valid in "proof" quality
+	nodeDimensionsIncludeLabels: false,
+	// Whether or not simple nodes (non-compound nodes) are of uniform dimensions
+	uniformNodeDimensions: false,
+	// Whether to pack disconnected components - valid only if randomize: true
+	packComponents: true,
 
-elements = elements.concat();
+	/* spectral layout options */
 
-console.log(elements[0]);
-console.log(blogPostToCyNodeArray(elements[0]));
-console.log(gon.related_posts);
+	// False for random, true for greedy sampling
+	samplingType: true,
+	// Sample size to construct distance matrix
+	sampleSize: 25,
+	// Separation amount between nodes
+	nodeSeparation: 75,
+	// Power iteration tolerance
+	piTol: 0.0000001,
+
+	/* incremental layout options */
+
+	// Node repulsion (non overlapping) multiplier
+	nodeRepulsion: 4500,
+	// Ideal edge (non nested) length
+	idealEdgeLength: 50,
+	// Divisor to compute edge forces
+	edgeElasticity: 0.45,
+	// Nesting factor (multiplier) to compute ideal edge length for nested edges
+	nestingFactor: 0.1,
+	// Maximum number of iterations to perform
+	numIter: 2500,
+	// For enabling tiling
+	tile: true,
+	// Represents the amount of the vertical space to put between the zero degree members during the tiling operation(can also be a function)
+	tilingPaddingVertical: 10,
+	// Represents the amount of the horizontal space to put between the zero degree members during the tiling operation(can also be a function)
+	tilingPaddingHorizontal: 10,
+	// Gravity force (constant)
+	gravity: 0.25,
+	// Gravity range (constant) for compounds
+	gravityRangeCompound: 1.5,
+	// Gravity force (constant) for compounds
+	gravityCompound: 1.0,
+	// Gravity range (constant)
+	gravityRange: 3.8,
+	// Initial cooling factor for incremental layout
+	initialEnergyOnIncremental: 0.3,
+
+	/* layout event callbacks */
+	ready: () => {}, // on layoutready
+	stop: () => {}, // on layoutstop
+};
 
 var cy = cytoscape({
 	container: $('#cy'),
-	elements: elements,
 });
 
-console.log(conversions);
-console.log(cy.elements()[0]);
-console.log(blogPostToCyNodeArray(cy.elements()[0]));
+cy.add(blogPostToCyNodeArray(gon.blog_post));
+gon.related_posts.forEach(function (post) {
+	cy.add(blogPostToCyNodeArray(post));
+});
+console.log(cy.elements());
+gon.internal_links.forEach(function (internalLink) {
+	cy.add(internalLinkToCyEdgeArray(internalLink));
+});
+
+cy.style({
+	style: {
+		selector: 'node[label]',
+		style: {
+			label: 'data(label)',
+		},
+	},
+});
+
+let layoutRunner = cy.layout(defaultfCoseOptions);
+let hasRun = layoutRunner.run();
 
 cy.on('tap', 'node', function (evt) {
 	var node = evt.target;
-	console.log('tapped ' + node.id());
 	location.href = '/blog_posts/' + node.id();
 });
