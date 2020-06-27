33c3110b92dba9e6a26089c51edbe9d152ed3cbf
secondbaker <secondbaker@gmail.com>
2020-06-27 15:26:07 -0400

    I realize now that CustomMarkdownRendere#link_internally should actually make the link, and there should be a separate recursive searching function.

--- app/lib/custom_markdown_renderer.rb
+++ app/lib/custom_markdown_renderer.rb
@@ -1,11 +1,25 @@
 class CustomMarkdownRenderer < Redcarpet::Render::HTML
     def paragraph text
-        pre_process("<p>#{text.strip}</p>\n")
+        "<p>#{pre_process(text.strip)}</p>\n"
     end
 
     private
 
     def pre_process text
-        return text.gsub('r', 'w')
+        internal_link_markers = text.scan(/\[\[.*\]\]/)
+        if internal_link_markers != []
+            text.scan(/\[\[.*\]\]/).map{|link| link_internally link[2..-3]}
+        else
+            text.gsub('r', 'w')
+        end
+    end
+
+    def link_internally link
+        internal_link_markers = link.scan(/\[\[.*\]\]/)
+        if internal_link_markers != []
+            link.scan(/\[\[.*\]\]/).map{|l| link_internally l[2..-3]}
+        else
+            'don\'t'
+        end
     end
 end
