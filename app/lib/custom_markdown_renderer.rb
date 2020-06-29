025e49f6829087e59c13cd178e1ef198d6dacbc4
secondbaker <secondbaker@gmail.com>
2020-06-29 16:35:07 -0400

    I'm going to push this, but then I'm going to branch and try something different.
    Here's what I think needs to happen:  text needs to be stored raw, which it is.  Any custom markdown things can happen on update and save.
    
    In particular, internal links should be changed into regular links to the pages they are connected to.
    
    However, when a note is deleted, references to it will need to be deleted from all other notes as well, turning the text from a link back into regular text.

--- app/lib/custom_markdown_renderer.rb
+++ app/lib/custom_markdown_renderer.rb
@@ -8,18 +8,24 @@ class CustomMarkdownRenderer < Redcarpet::Render::HTML
     def pre_process text
         internal_link_markers = text.scan(/\[\[.*\]\]/)
         if internal_link_markers != []
-            text.scan(/\[\[.*\]\]/).map{|link| link_internally link[2..-3]}
+            text.scan(/\[\[.*\]\]/).map{|link| search_for_links link[2..-3]}
         else
             text.gsub('r', 'w')
         end
     end
 
-    def link_internally link
+    def search_for_links link
         internal_link_markers = link.scan(/\[\[.*\]\]/)
         if internal_link_markers != []
-            link.scan(/\[\[.*\]\]/).map{|l| link_internally l[2..-3]}
+            link.scan(/\[\[.*\]\]/).map{|l| search_for_links l[2..-3]}
         else
             'don\'t'
         end
+        link_internally link
+    end
+
+    def link_internally link
+        link_to = "blog_post/#{link}"
+        return "<a href=#{link_to}><a>"
     end
 end
