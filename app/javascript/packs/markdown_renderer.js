bad9f0adf0e12f4949ffa1d07445afb8874b1040
secondbaker <secondbaker@gmail.com>
2020-06-30 09:12:42 -0400

    Okay, we do find all the posts correctly.  Next we just need to put the sanitized HTML that was converted from Markdown into them.

--- app/javascript/packs/markdown_renderer.js
+++ app/javascript/packs/markdown_renderer.js
@@ -3,4 +3,5 @@ let marked = require('marked');
 
 gon.blog_posts.map((post) => {
     console.log(DOMPurify.sanitize(marked(post.body)));
+    document.getElementById('post-body-' + post.id).innerHTML = post.id
 });
