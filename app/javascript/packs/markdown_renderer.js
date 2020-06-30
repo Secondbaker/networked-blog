cacfbbcc960d9572628848e33d426351e0fb41f3
secondbaker <secondbaker@gmail.com>
2020-06-30 10:45:53 -0400

    The secret was nested spans.  That's how it had to work the whole time.

--- app/javascript/packs/markdown_renderer.js
+++ app/javascript/packs/markdown_renderer.js
@@ -1,9 +1,18 @@
 import DOMPurify from 'dompurify';
 let marked = require('marked');
+import TurndownService from 'turndown';
+var turndownService = new TurndownService();
 
 const renderer = {
         link(href, title, text) {
-        return `<span class='link' data-link-target='test.com' data-link-text='test'>${text}</span>`;
+            console.log(text);
+            var targetText = text;
+            if(text.match('(<span.*</span>)') != null)
+            {
+                console.log('here');
+                targetText = turndownService.turndown(text);
+            }
+        return `<span class='link' data-link-target='${href}' data-link-text='${targetText}'>${text}</span>`;
     }
 }
 
@@ -13,12 +22,6 @@ gon.blog_posts.map((post) => {
     document.getElementById('post-body-' + post.id).innerHTML = DOMPurify.sanitize(marked(post.body));
 });
 
-console.log($('span.link'));
-$('span.link').each(function (index){
-    console.log($(this).children());
-    if ($(this).children().length == 0)
-    {
-        console.log('no one');
-    }
+$('span.link').on('click', function (e){
+    window.location =$(e.target).data('link-target');
 })
-
