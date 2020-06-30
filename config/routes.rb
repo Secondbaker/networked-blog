b47575a485e42d8057840090aa32f11fa1fcbac8
secondbaker <secondbaker@gmail.com>
2020-06-30 11:47:15 -0400

    Wait this should be in the controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :blog_posts do
-    
   end
   root to: 'blog_posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
