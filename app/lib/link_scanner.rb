cb5446aee5c8374b617f3df3908ab2daf52c3004
Austin Baker <secondbaker@Austins-MacBook.local>
2020-07-04 15:55:51 -0400

    Alright, this seems to be working so far.

--- app/lib/link_scanner.rb
+++ app/lib/link_scanner.rb
@@ -1,6 +1,26 @@
 class LinkScanner
+    
     attr_accessor :text
     def initialize(text)
         @text = text
+    end
+    def render
+        output = ''
+        index = 0
+        while index < text.length do
+            if text[index..index + 1] == '[['
+                output += 'whoa this syntax is easier'
+                link_start = index
+                link_end = text.index(']]', index)  + 1
+                link = text[link_start..link_end]
+
+                index += link.length
+
+            else
+                output += text[index]
+                index += 1
+            end
+        end
+        output
     end
 end
