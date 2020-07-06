32009c9c0dddcf0000c8a946ee76ce99608bf389
secondbaker <secondbaker@gmail.com>
2020-07-06 09:30:14 -0400

    The edit request is correct, so that's pretty cool.

--- app/javascript/packs/edit_mode.js
+++ app/javascript/packs/edit_mode.js
@@ -3,5 +3,9 @@ console.log('edit_mode');
 $('.text-block').click( function (event) {
         console.log('here');
         console.log(event.target.id);
+        myID = event.target.id.split('-')[2]
+        $.ajax( { 
+            url: `/text_blocks/${myID}/edit` 
+        })
     }
 );
