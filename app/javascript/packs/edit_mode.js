fa5fc3d86e85a4c457fefa23d6675e0b2711ba16
secondbaker <secondbaker@gmail.com>
2020-07-06 09:39:40 -0400

    I named the TextBlock's on click function.

--- app/javascript/packs/edit_mode.js
+++ app/javascript/packs/edit_mode.js
@@ -1,11 +1,14 @@
 console.log('edit_mode');
 
-$('.text-block').click( function (event) {
-        console.log('here');
-        console.log(event.target.id);
-        myID = event.target.id.split('-')[2]
-        $.ajax( { 
-            url: `/text_blocks/${myID}/edit` 
-        })
-    }
-);
+function editMode (event) {
+    console.log('here');
+    console.log(event.target.id);
+    myID = event.target.id.split('-')[2]
+    $.ajax({
+        url: `/text_blocks/${myID}/edit`
+    })
+}
+
+$('.text-block').click( editMode );
+
+$('.text-block-container').click()
