a8aa6c0c07fbe20d1bfb6023fff16b4a03743d45
secondbaker <secondbaker@gmail.com>
2020-07-06 10:40:41 -0400

    Ooh I did some destructuring.  So fancy.

--- app/javascript/packs/edit_mode.js
+++ app/javascript/packs/edit_mode.js
@@ -2,6 +2,7 @@ console.log('edit_mode');
 
 // sets event.target to edit mode
 function editMode (event) {
+    console.log('editMode')
     thisBlock = event.target
     
     //just so we don't need to make additional requests for edit mode
@@ -9,6 +10,9 @@ function editMode (event) {
     {
         return;
     }
+
+    console.log($('.editing'))
+
     console.log('here');
     console.log(thisBlock.id);
     $(thisBlock).addClass('editing');
@@ -22,9 +26,13 @@ function editMode (event) {
 
 //sets everything but event.target to read mode
 function readMode (event) {
-
+    let { target } = event;
+    console.log('readMode');
+    console.log($('.editing'));
+    console.log('readMode target ' + target);
+    
 }
 
 $('.text-block').click( editMode );
 
-$('.text-block-container').click()
+$('.text-block-container').click( readMode )
