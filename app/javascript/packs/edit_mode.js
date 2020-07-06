34604b0c90cc035f4475b4e52f1de7951cb90ac4
secondbaker <secondbaker@gmail.com>
2020-07-06 09:45:03 -0400

    Now only makes requests for edit mode if it's not already in edit mode.

--- app/javascript/packs/edit_mode.js
+++ app/javascript/packs/edit_mode.js
@@ -1,12 +1,26 @@
 console.log('edit_mode');
 
+// sets event.target to edit mode
 function editMode (event) {
+    thisBlock = event.target
+    
+    //just so we don't need to make additional requests for edit mode
+    if ($(thisBlock).hasClass('editing'))
+    {
+        return;
+    }
     console.log('here');
     console.log(event.target.id);
+    $(event.target).addClass('editing');
     myID = event.target.id.split('-')[2]
     $.ajax({
         url: `/text_blocks/${myID}/edit`
     })
+}
+
+//sets everything but event.target to read mode
+function readMode (event) {
+
 }
 
 $('.text-block').click( editMode );
