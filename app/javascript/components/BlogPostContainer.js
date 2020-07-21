0dd0585c24999543d3ea62c80b1df80d5216e324
Austin Baker <secondbaker@Austins-MacBook.local>
2020-07-20 22:13:25 -0400

    Got React set up properly. Also completely broke JQuery, so it's React all the way now!

--- app/javascript/components/BlogPostContainer.js
+++ app/javascript/components/BlogPostContainer.js
@@ -1,10 +1,11 @@
 import React from "react"
 import PropTypes from "prop-types"
+import BlogPost from "./BlogPost"
 class BlogPostContainer extends React.Component {
+  
   render () {
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <BlogPost />
     );
   }
 }
