887c3316f3ea0414f3b9312c9dcd790ee6aeb050
Austin Baker <secondbaker@gmail.com>
2020-08-22 17:31:06 -0400

    Set up an onload function for the graph, which will be used to set it to the right size.

--- app/javascript/components/Graph.js
+++ app/javascript/components/Graph.js
@@ -5,6 +5,8 @@ import CytoscapeComponent from 'react-cytoscapejs';
 class Graph extends React.Component {
     constructor(props) {
         super(props);
+        console.log(props.message);
+        this.handleLoad = this.handleLoad.bind(this);
     }
 
     render() {
@@ -14,7 +16,20 @@ class Graph extends React.Component {
             { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
         ];
 
-        return <CytoscapeComponent elements={elements} className='width-100 height-100' />;
+        return <CytoscapeComponent elements={elements} className="width-100% height-100%" style={{ height: "600px", width: "600px" }} />;
+    }
+
+    componentDidMount() {
+        window.addEventListener('load', this.handleLoad);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('load', this.handleLoad)
+    }
+
+    handleLoad() {
+        console.log('Handle Load');
+
     }
 }
 
