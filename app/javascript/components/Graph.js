a47573f709cb9d7ec5452f182450ffa462bb59cd
secondbaker <secondbaker@gmail.com>
2020-08-31 11:03:00 -0400

    Working on getting the nodes from Rails to React

--- app/javascript/components/Graph.js
+++ app/javascript/components/Graph.js
@@ -1,25 +1,44 @@
+import Cytoscape from 'cytoscape';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
+import coseBilkent from 'cytoscape-cose-bilkent';
+import { nodeName } from 'jquery';
+const cy = Cytoscape;
+
+cy.use(coseBilkent);
 
 class Graph extends React.Component {
+
     constructor(props) {
         super(props);
-        console.log(props.message);
+        console.log(props.nodes);
         this.handleLoad = this.handleLoad.bind(this);
         this.state = { height: "600px", width: "600px" };
     }
 
     render() {
-        const elements = [
-            { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-            { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-            { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-        ];
+
+        var elements = [];
+        var nodes = this.props.nodes.slice();
+        var mappedNodes = nodes.map((node) => {
+            console.log({ data: { id: node.id, label: node.name } });
+            return { data: { id: node.id, label: node.name } };
+        });
+        console.log(mappedNodes);
+        elements.concat();
+        console.log(elements);
+        const layout = {
+            name: 'cose-bilkent',
+            animate: 'end',
+            animationEasing: 'ease-out',
+            animationDuration: 1000,
+            randomize: true
+        };
 
         return (
 
-            <CytoscapeComponent elements={elements} className="width-100% height-100% blur" style={{ height: this.state.height, width: this.state.width, filter: blur("20px") }} />
+            <CytoscapeComponent layout={layout} elements={elements} className="width-100% height-100% blur" style={{ height: this.state.height, width: this.state.width, filter: blur("20px") }} />
 
         );
     }
@@ -34,6 +53,7 @@ class Graph extends React.Component {
 
     handleLoad() {
         console.log('Handle Load');
+        console.log(this.props.nodes);
         this.setState({ height: "100%", width: "100%" });
     }
 }
