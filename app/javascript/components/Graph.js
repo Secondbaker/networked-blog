e557e3c360e44820ed1b897dd3a1216ad6719448
secondbaker <secondbaker@gmail.com>
2020-09-02 09:26:36 -0400

    I think I got it working.  I'm going to seed the database a few times to test it.

--- app/javascript/components/Graph.js
+++ app/javascript/components/Graph.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
 import coseBilkent from 'cytoscape-cose-bilkent';
 import { nodeName } from 'jquery';
+import Spread from 'cytoscape-spread';
 Cytoscape.use(coseBilkent);
+Spread(Cytoscape);
 
 class Graph extends React.Component {
 
@@ -36,51 +38,52 @@ class Graph extends React.Component {
 
     handleLoad() {
         console.log('Handle Load');
-        console.log(this.props.nodes);
+        //console.log(this.props.nodes);
         var height = "100%";
         var width = "100%";
         //this.cy.animate({})
         var elements = [];
         var nodes = this.props.nodes.slice();
         var mappedNodes = nodes.map((node) => {
-            console.log({ data: { id: node.id, label: node.name } });
+            //console.log({ data: { id: node.id, label: node.name } });
             return { data: { id: node.id.toString(), label: node.name } };
         });
         elements = elements.concat(mappedNodes);
 
         var edges = this.props.edges.slice();
-        console.log(edges);
+        //console.log(edges);
         var mappedEdges = edges.map((edge) => {
-            console.log({ data: { source: edge.source_id.toString(), target: edge.destination_id.toString() } });
+            //console.log({ data: { source: edge.source_id.toString(), target: edge.destination_id.toString() } });
             return { data: { source: edge.source_id.toString(), target: edge.destination_id.toString() } };
         });
         elements = elements.concat(mappedEdges);
 
         const layout = {
             name: 'cose-bilkent',
-            randomize: true
+            randomize: true,
+            animate: true, // Whether to show the layout as it's running
+            ready: undefined, // Callback on layoutready
+            stop: undefined, // Callback on layoutstop
+            fit: true, // Reset viewport to fit default simulationBounds
+
+            minDist: 20, // Minimum distance between nodes
+            padding: 20, // Padding
+            expandingFactor: -1.0, // If the network does not satisfy the minDist
+            // criterium then it expands the network of this amount
+            // If it is set to -1.0 the amount of expansion is automatically
+            // calculated based on the minDist, the aspect ratio and the
+            // number of nodes
+            prelayout: { name: 'cose' }, // Layout options for the first phase
+            maxExpandIterations: 4, // Maximum number of expanding iterations
+            boundingBox: undefined, // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+            randomize: true // Uses random initial node positions on true
         };
-
-        var animation = this.cy.animation({
-            duration: 1000,
-            queue: true
-        });
-
-        this.cy.animation({
-            duration: 1000,
-            queue: true,
-            fit: {
-                padding: 20
-            }
-        })
-
-        console.log(animation);
 
         this.setState({ height: height, width: width, layout: layout, elements: elements.slice() });
 
-        animation.play();
 
         console.log('Load handled');
+        this.centerGraph();
     }
 
     centerGraph() {
