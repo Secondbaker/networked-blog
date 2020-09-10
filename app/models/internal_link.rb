c60a75855d79b20c912df5dd1aa7c118d111ddbf
secondbaker <secondbaker@gmail.com>
2020-09-10 09:14:49 -0400

    TextBlocks can now link to BlogPosts on the back end.  Now it's time to give InternalLink the ability to interpret those links in text.

--- app/models/internal_link.rb
+++ app/models/internal_link.rb
@@ -1,5 +1,5 @@
 class InternalLink < ApplicationRecord
-    belongs_to :source, class_name: 'BlogPost', optional: true
+    belongs_to :source, class_name: 'TextBlock', optional: true
     belongs_to :destination, class_name: 'BlogPost', optional: true, counter_cache: true
     before_create :set_destination_name
 
@@ -52,6 +52,12 @@ class InternalLink < ApplicationRecord
         end
         text
     end
+
+    #scans the text_block for text that looks like internal links
+    def self.convertToIDs text_block
+        
+    end
+
     private 
     def set_destination_name
         self.destination_name = self.destination.name
--- app/models/text_block.rb
+++ app/models/text_block.rb
@@ -1,7 +1,8 @@
 class TextBlock < ApplicationRecord
     belongs_to :blog_post
     acts_as_list scope: :blog_post
-
+    has_many :internal_links, foreign_key: 'source_id', dependent: :destroy
+    has_many :destinations, through: :internal_links, foreign_key: 'destination_id', class_name: 'BlogPost'
     before_save :check_for_links
 
     def check_for_links
